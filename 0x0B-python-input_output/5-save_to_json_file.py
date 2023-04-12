18f0a3ce2bff38982f4f10485a96d6affe77df78
Your Nameyohbi2022git config user.name git clone https://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@gitclearroot@c174ab32603f:/alx-pre_course# git config --global user.name Your <ybirara@yahoo.com>
2023-04-11 23:12:35 -0700

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,7 +4,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-"""Write an object to a text file using JSON representation."""
+    """Write an object to a text file using JSON representation."""
     with open(filename, "w") as f:
         json.dump(my_obj, f)
 
